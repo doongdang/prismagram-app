3a4d2ce25c6451f912f50c101d45596e9d25a648
SeokHyun Cho <doongdang@naver.com>
2020-04-19 16:40:59 +0900

    #10.2

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
 import * as Font from "expo-font";
-import { Text, View } from "react-native";
+import { Text, View, AsyncStorage } from "react-native";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import apolloClientOptions from "./apollo";
 
 export default function App() {
-  const [loaded, setLoaded] = useState(false);
+  const [loaded, setLoaded] = useState(false); // loaded 상태 변환
+  const [client, setClient] = useState(null);
 
   const preLoad = async () => {
     try {
       await Font.loadAsync({
         ...Ionicons.font,
       });
+      await Asset.loadAsync([require("./assets/logo.png")]);
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage,
+      });
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions,
+      });
       setLoaded(true);
+      setClient(client);
     } catch (e) {
       console.log(e);
     }
@@ -22,11 +41,15 @@ export default function App() {
     preLoad();
   }, []);
 
-  return loaded ? (
-    <View style={{ justifyContent: "center", flex: 1, alignItems: "center" }}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View> // 로딩되면 화면에 보여짐
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <View style={{ justifyContent: "center", flex: 1, alignItems: "center" }}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </ApolloProvider>
   ) : (
     <AppLoading /> // 로딩중인 동안 보여지는 Splash Screens
   );
 }
+// 처음에 Component Monut 되면 loading = false / client  =null 상태
+//
