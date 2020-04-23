0498cc406b71676f5ad81294466e861884ccc9e1
SeokHyun Cho <doongdang@naver.com>
2020-04-23 14:44:17 +0900

    #12.9

--- hooks/useInput.js
+++ hooks/useInput.js
@@ -7,7 +7,7 @@ const useInput = (initialValue) => {
     setValue(text);
   };
   // 처음에 initialValue로 value 값을 초기화 해두고 text를 받아와서 onChange 함수를 통해 value값을 text로 설정해줌
-  return { value, onChange };
+  return { value, onChange, setValue };
 };
 
 export default useInput;
--- screens/Auth/SignUp.js
+++ screens/Auth/SignUp.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import * as Facebook from "expo-facebook";
 import styled from "styled-components";
 import AuthButton from "../../components/AuthButton";
 import AuthInput from "../../components/AuthInput";
@@ -11,6 +12,14 @@ const View = styled.View`
   justify-content: center;
   align-items: center;
   flex: 1;
+`;
+
+const FBContainer = styled.View`
+  margin-top: 25px;
+  padding-top: 25px;
+  border-top-width: 1px;
+  border-top-color: ${(props) => props.theme.lightGreyColor};
+  border-style: solid;
 `;
 
 export default ({ navigation, route }) => {
@@ -64,6 +73,31 @@ export default ({ navigation, route }) => {
     }
   };
 
+  const fbLogIn = async () => {
+    try {
+      setLoading(true);
+      await Facebook.initializeAsync("1538847239622072");
+      const { type, token } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ["public_profile", "email"],
+      });
+      if (type === "success") {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(
+          `https://graph.facebook.com/me?access_token=${token}&fields=id,last_name,first_name,email`
+        );
+        const { email, first_name, last_name } = await response.json();
+        emailInput.setValue(email);
+        fNameInput.setValue(first_name);
+        lNameInput.setValue(last_name);
+        setLoading(false);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  };
+
   return (
     <TouchableWithoutFeedback disabled={loading} onPress={Keyboard.dismiss}>
       <View>
@@ -91,6 +125,14 @@ export default ({ navigation, route }) => {
           onSubEditing={handleSignUp} // 보내기버튼 클릭시 handleSignUp 실행
         />
         <AuthButton loading={loading} onPress={handleSignUp} text={"Sign Up"} />
+        <FBContainer>
+          <AuthButton
+            bgColor={"#2172E8"}
+            loading={false}
+            onPress={fbLogIn}
+            text={"Connect FaceBook"}
+          />
+        </FBContainer>
       </View>
     </TouchableWithoutFeedback>
   );
